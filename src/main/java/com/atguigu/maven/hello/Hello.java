8f98bc7aecb25e5c73796dc1bb802d4ac90aba0e
Yhfme <53857409+Yhfme@users.noreply.github.com>
2019-08-08 20:35:44 +0800

    update hello.java2

--- src/main/java/com/atguigu/maven/hello/Hello.java
+++ src/main/java/com/atguigu/maven/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public void sayhello(String name) {
 		System.out.println("hello"+name);
                 System.out.println("first time");
+		System.out.println("second time");
 	}
 }
