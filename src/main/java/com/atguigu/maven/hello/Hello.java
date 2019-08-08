e11c711d39ced333b3dbb754b547643426822f34
Yhfme <53857409+Yhfme@users.noreply.github.com>
2019-08-08 20:33:16 +0800

    update hello.java

--- src/main/java/com/atguigu/maven/hello/Hello.java
+++ src/main/java/com/atguigu/maven/hello/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.maven.hello;
 public class Hello {
 	public void sayhello(String name) {
 		System.out.println("hello"+name);
+                System.out.println("first time");
 	}
 }
